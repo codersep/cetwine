f179c24a96a09b8567ab7661956a95a4c280439e
coderwine <coderwine@163.com>
2020-11-14 21:02:02 +0800

    项目初始化

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/commen',
+        'comments': '@/comments',
+        'network': '@/network',
+        'views': '@/views',
+
+      }
+    }
+  }
+
+}
